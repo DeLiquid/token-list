670e1506b754fa933f94fd7634eaf31adbca3ec5
Kailing Ding <markdingkl@gmail.com>
2022-06-02 18:16:54 -0700

    Create tokenlist.spec.ts

---
+++ tests/tokenlist.spec.ts
@@ -0,0 +1,26 @@
+import { TokenList, ChainList, FetchStrategy } from '../src';
+
+describe('Test TokenList class', () => {
+  test('TokenList can get tokens from all the chains', async () => {
+    const tokenList = new TokenList();
+    const tokens = await tokenList.getAllTokens();
+    const actualChains = [...new Set(tokens.map((token) => token.chain))];
+    expect(actualChains).toEqual(ChainList);
+  });
+
+  test('TokenList can get all tokens on Solana blockchain', async () => {
+    const tokenList = new TokenList();
+    const chains = ['solana'];
+    const tokens = await tokenList.getTokensByChains(chains);
+    const actualChains = [...new Set(tokens.map((token) => token.chain))];
+    expect(actualChains).toEqual(chains);
+  });
+
+  test('TokenList can get all tokens on both Solana and Ethereum blockchains', async () => {
+    const tokenList = new TokenList();
+    const chains = ['solana', 'ethereum'];
+    const tokens = await tokenList.getTokensByChains(chains);
+    const actualChains = [...new Set(tokens.map((token) => token.chain))];
+    expect(actualChains).toEqual(chains);
+  });
+});
